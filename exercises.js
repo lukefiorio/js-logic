fd07e46e72739e9bcd6164070c76cd23fcaee3e8
Luke Fiorio <lukefiorio@Lukes-MacBook-Pro.local>
2019-01-13 14:41:37 -1000

    added func daClub

--- exercises.js
+++ exercises.js
@@ -245,6 +245,14 @@ console.log('even:',evenStevens(6003));
  * Console.log your result.
 */ 
 
+function daClub (cover,age) {
+  if (cover>=21 && age>=21) {
+    return "Welcome to the Legends Lounge."
+  } else {
+    return "Chuck E Cheese is across the street."
+  }
+}
+console.log('club entry:',daClub(35,20));
 
 /*
  * #12
